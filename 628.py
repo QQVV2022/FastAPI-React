0a2e2694b9a03461b49624798e95f6f00339e852
Ken Comvandee <ncomvandee@Kens-MacBook-Pro-2.local>
2022-06-28 11:41:33 -0700

    Add line by Ken

--- 628.py
+++ 628.py
@@ -7,4 +7,7 @@ print('This is a code from Ken')
 # Ken: type whatever you want on Line 5
 
 # Qing: I will type on Line 3 or 5 to make conflict
->>>>>>> c0967f2492baf7a2bc4a7a7501053469890f3a74
+>>>>>>> c0967f2492baf7a2bc4a7a7501053469890f3a7
+
+
+# Try again by Ken
