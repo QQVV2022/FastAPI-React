5c92678300823b5f9bfb944811cf82e3e0ea9c9e
trang2020 <tranthutrang1985@gmail.com>
2022-06-28 12:20:11 -0700

    Created conflict in Ken branch

--- 628.py
+++ 628.py
@@ -4,6 +4,7 @@ print('This is a code from Ken')
 =======
 # Trang: type whatever you want on Line 3
 1234
+print("Create conflict in the same branch")
 # Ken: type whatever you want on Line 5
 
 # Qing: I will type on Line 3 or 5 to make conflict
